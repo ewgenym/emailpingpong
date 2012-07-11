ed47bbde483338925f7b707a28e8d694f55472b9
ewgenym <maksak@yandex.ru>
2012-07-11 15:41:13 +0300

    ReShareper

---
+++ Source/_ReSharper.EmailPingPong/Decompiler/metadata/23/1fdfa3ab/TreeDerivationMode.cs
@@ -0,0 +1,21 @@
+﻿// Type: DevExpress.Xpf.Grid.TreeDerivationMode
+// Assembly: DevExpress.Xpf.Grid.v11.2, Version=11.2.11.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a
+// Assembly location: C:\Program Files (x86)\DevExpress 2011.2\Components\Bin\Framework\DevExpress.Xpf.Grid.v11.2.dll
+
+namespace DevExpress.Xpf.Grid
+{
+  /// <summary>
+  /// 
+  /// <para>
+  /// Lists values that specify the tree derivation mode.
+  /// 
+  /// </para>
+  /// 
+  /// </summary>
+  public enum TreeDerivationMode
+  {
+    Selfreference,
+    ChildNodesSelector,
+    HierarchicalDataTemplate,
+  }
+}
