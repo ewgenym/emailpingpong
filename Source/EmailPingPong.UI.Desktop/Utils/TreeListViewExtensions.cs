bb42ecafe664b70c9d7f9ba8c004ee66541074ba
ewgenym <maksak@yandex.ru>
2012-07-17 20:52:52 +0300

    Preserve nodes state after each folder\search mode swith and restore it back when neccessary

---
+++ Source/EmailPingPong.UI.Desktop/Utils/TreeListViewExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using DevExpress.Xpf.Grid;
+using DevExpress.Xpf.Grid.TreeList;
+
+namespace EmailPingPong.UI.Desktop.Utils
+{
+	public static class TreeListViewExtensions
+	{
+		public static TreeListDataProvider GetTreeListDataProvider(this TreeListView treeListView)
+		{
+			var treeListViewType = treeListView.GetType();
+			var propertyInfo = treeListViewType.GetProperty("TreeListDataProvider",
+															BindingFlags.GetProperty | BindingFlags.Instance |
+															BindingFlags.NonPublic);
+			return (TreeListDataProvider)propertyInfo.GetValue(treeListView, null);
+		}
+	}
+}
